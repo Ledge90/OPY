dcb50d2beb31bd9e3c6f182ebb10b8787d9be144
Ledge90 <92630356+Ledge90@users.noreply.github.com>
2021-10-16 14:58:45 +0300

    Update Task3.cs

--- tasks/Task3.cs
+++ tasks/Task3.cs
@@ -5,11 +5,11 @@ class HelloWorld
     
   static void Main(string[] args)
   {
-    int x = Convert.ToInt(Console.ReadLine());
+    int x = Convert.ToInt(Console.ReadLine()); # объявляем  переменные
     int y = Convert.ToInt(Console.ReadLine());
     int z = Convert.ToInt(Console.ReadLine());
     
-    if (1 <= x <= 3)  
+    if (1 <= x <= 3)  # проверяем условие, и если оно истинно то выводим его
     {
         Console.WriteLine(x);
     }
