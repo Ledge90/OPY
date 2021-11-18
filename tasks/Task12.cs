9706ed18c9c97144bab51e4e3cbae4d9daf759e0
Ledge90 <92630356+Ledge90@users.noreply.github.com>
2021-11-18 22:29:40 +0300

    Update Task12.cs

--- tasks/Task12.cs
+++ tasks/Task12.cs
@@ -9,13 +9,11 @@ namespace Task11
         {
             Console.Write("Enter the length of the array: ");
             int x = Convert.ToInt32(Console.ReadLine());
-
             int[] b = new int[x];
 
             for (int i = 0; i < b.Length; i++)
             {
                 Console.Write($"Enter {i} element: ");
-
                 b[i] = Convert.ToInt32(Console.ReadLine());
             }
 
