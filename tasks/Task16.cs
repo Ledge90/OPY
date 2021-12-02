a08eca338f7abbdb5d16859de5347b4c629b26a4
Ledge90 <92630356+Ledge90@users.noreply.github.com>
2021-12-02 12:59:18 +0300

    Add files via upload

---
+++ tasks/Task16.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perimetr
+{
+    internal class Program
+    {
+        static int Perimetr(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите три стороны треугольника: ");
+                int katet1 = int.Parse(Console.ReadLine());
+                int katet2 = int.Parse(Console.ReadLine());
+                int resolt = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Периметр равен: {Perimetr(katet1, katet2, resolt)}");
+            }
+        }
+    }
+}
