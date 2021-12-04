776cfe021b9626ab96a9220dead8ad00447c8aff
Ledge90 <92630356+Ledge90@users.noreply.github.com>
2021-12-04 12:35:48 +0300

    Update Task18.cs

--- tasks/Task18.cs
+++ tasks/Task18.cs
@@ -1,6 +1,10 @@
+// Параметр Ref (параметры ввода-вывода) Выполнить: Создайте функцию MinmaxSeq(), которая принимает два аргумента по ссылке (ref аргументы)
+// — минимальное и максимальное значение вводимой последовательности чисел. Запросите у пользователя ввести последовательность, которая должна
+// закончиться 0 (если вводится 0, то ввод завершается). Функция должна выполнять поиск максимального и минимального значения последовательности и возвращать результат.
+
 using System;
 
-namespace Exercise3
+namespace task18
 {
     internal class Program
     {
@@ -8,15 +12,15 @@ namespace Exercise3
         {
             int? min = null, max = null;
             MinmaxSeq(ref min, ref max);
-            Console.WriteLine($"min: {min}\nmax:{max}");
+            Console.WriteLine($"min: {min}\n max:{max}");
         }
 
         private static void MinmaxSeq(ref int? min, ref int? max)
         {
-            Console.WriteLine("Введите полседовательность и закончите ввод нулем:");
+            Console.WriteLine("Enter half-sequence and end with zero:");
             min = max = int.Parse(Console.ReadLine());
             
-            if (min == 0) return; // проверка на то, что пользователь первым числом ввел 0
+            if (min == 0) return;
             
             var x = int.Parse(Console.ReadLine());
             while (x != 0)
